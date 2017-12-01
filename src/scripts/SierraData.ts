f7cd8b2005cac41d8345bd34d469465d8cbfb746
Sean Johnson <sjohnson@sjohnsonaz.com>
2017-11-30 21:24:11 -0700

    Fixed package exports.

--- src/scripts/SierraData.ts
+++ src/scripts/SierraData.ts
@@ -1,6 +1,6 @@
 export { IData } from './interfaces/IData';
 export { IMethods } from './interfaces/IMethods';
-import { IQuery } from './interfaces/IQuery';
+export { IQuery } from './interfaces/IQuery';
 export { IQueryResult } from './interfaces/IQueryResult';
 export { SchemaModel, Schema } from './implementations/SchemaModel';
 export { default as Gateway } from './implementations/Gateway';
