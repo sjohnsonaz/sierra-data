1d9136bf38117f92da016207562cade506c2cb12
Sean Johnson <sjohnson@sjohnsonaz.com>
2019-10-19 14:23:21 -0700

    Fixed Model test.

--- src/tests/test.ts
+++ src/tests/test.ts
@@ -6,8 +6,18 @@ import CollectionFactory from '../scripts/implementations/CollectionFactory';
 import { prop } from '../scripts/implementations/Decorators';
 import { Collection, IClientData } from '../scripts/SierraData';
 
-describe('Model.unwrap', () => {
-    it('should create an object with all valid properties', async () => {
+describe('Model', function () {
+    let collectionFactory = new CollectionFactory();
+
+    before(async function () {
+        await collectionFactory.connect('mongodb://localhost:27017', 'sierra-data');
+    });
+
+    after(async function () {
+        await collectionFactory.close();
+    });
+
+    it('should create an object with all valid properties', async function () {
         interface IParent {
             parentValue: string;
         }
@@ -34,28 +44,21 @@ describe('Model.unwrap', () => {
             numberValue: number;
         }
 
-        let collectionFactory = new CollectionFactory();
-        try {
-            await collectionFactory.connect('mongodb://localhost:27017', 'sierra-data');
-            let testCollection = collectionFactory.getCollection('testcollection');
-            let collection = new Collection<TestModel>(testCollection, TestModel);
+        let testCollection = collectionFactory.getCollection('testcollection');
+        let collection = new Collection<TestModel>(testCollection, TestModel);
 
-            let testModel = collection.create();
-            testModel.fromClient({});
-            await testModel.save();
-            // await collection.insert(testModel);
-            expect(testModel.stringValue).to.equal('abcd');
+        let testModel = collection.create();
+        testModel.fromClient({});
+        await testModel.save();
+        // await collection.insert(testModel);
+        expect(testModel.stringValue).to.equal('abcd');
 
-            testModel.stringValue = 'efgh';
-            await testModel.save();
-            // await collection.update(testModel._id, testModel);
+        testModel.stringValue = 'efgh';
+        await testModel.save();
+        // await collection.update(testModel._id, testModel);
 
-            let result = await collection.get(testModel._id);
-            collection.findOne({_id: testModel._id})
-            expect(result.stringValue).to.equal('efgh');
-        }
-        finally {
-            collectionFactory.close();
-        }
+        let result = await collection.get(testModel._id);
+        collection.findOne({ _id: testModel._id })
+        expect(result.stringValue).to.equal('efgh');
     });
 });
